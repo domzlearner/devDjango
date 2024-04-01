73eea09ba415f168e37bbbf72162c4dcdb77b40c
Domz Condes <domz.learner@gmail.com>
2024-04-02 06:25:50 +0800

    Additional feature for weather included

--- main/views.py
+++ main/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
+import requests 
+from bs4 import BeautifulSoup
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'main/home.html')
+    city = "Santa Rosa"
+    
+    # url and requests 
+    url = "https://www.google.com/search?q=" + "weather" + city 
+    html = requests.get(url).content 
+    
+    # getting raw data 
+    soup = BeautifulSoup(html, 'html.parser') 
+    temperature = soup.find('div', 
+                            attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text 
+    time_sky = soup.find('div',  
+                        attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text 
+    
+    # formatting data 
+    sky = time_sky.split('\n')[1]
+
+    data = {
+        "sky": str(sky),
+        "temp": str(temperature),
+        "city": str(city)
+    }
+    
+    print(data)
+
+    return render(request, 'main/home.html', data)
